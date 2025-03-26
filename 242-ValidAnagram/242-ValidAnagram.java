d41dcbc49ae0cc963d5cead6e0c6fbba1c047848
Bsahoo99 <144132188+Bsahoo99@users.noreply.github.com>
2025-03-26 17:29:33 -0500

    Create file

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,19 @@
+// Last updated: 3/26/2025, 5:29:32 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) return false;
+        }
+
+        return true;
+    }
+}
