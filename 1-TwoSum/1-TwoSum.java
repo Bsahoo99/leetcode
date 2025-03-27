6d08af400beee020716355b58d301d2ca1fda4c3
Bsahoo99 <144132188+Bsahoo99@users.noreply.github.com>
2025-03-26 19:21:55 -0500

    File updated at 3/26/2025, 7:21:54 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,13 +1,19 @@
-// Last updated: 3/26/2025, 7:21:32 PM
+// Last updated: 3/26/2025, 7:21:54 PM
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+    HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (prevMap.containsKey(diff)) {
+                return new int[] { prevMap.get(diff), i };
             }
+
+            prevMap.put(num, i);
         }
-        return new int[0];
+
+        return new int[] {};
     }
 }
