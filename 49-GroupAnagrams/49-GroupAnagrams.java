d51384e738de7c6644df45b2f4c1bb0960ba01f0
Bsahoo99 <144132188+Bsahoo99@users.noreply.github.com>
2025-03-28 21:05:01 -0500

    Create file

---
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -0,0 +1,14 @@
+// Last updated: 3/28/2025, 9:05:01 PM
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> res = new HashMap<>();
+        for (String s : strs){
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedS = new String(charArray);
+            res.putIfAbsent(sortedS,new ArrayList<>());
+            res.get(sortedS).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
